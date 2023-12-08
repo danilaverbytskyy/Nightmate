ee7faedcedf60b96c733e4fbf93d80b1e7ec1739
Danila <dod.shreder@gmail.com>
2023-12-08 23:47:34 +0300

    initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import './bootstrap';
+import 'https://cdn.startbootstrap.com/sb-forms-latest.js';
+import 'https://kit.fontawesome.com/ea30508f49.js';
+import '../assets/bootstrap/js/bootstrap.bundle.min.js';
+import '../assets/bootstrap/js/bootstrap.min.js';
+import './main.js';
