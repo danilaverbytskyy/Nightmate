83b86a732bb33e0e4e7a1c1452ee0f4b761f69f8
Danila <dod.shreder@gmail.com>
2024-02-15 23:19:01 +0300

    made select in dashboard

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -211,4 +211,22 @@
             </div>
         </div>
     </div>
+    <script>
+      function filterDreamsByMonth () {
+        let selectedMonth = parseInt(document.getElementById('month-select').value)
+
+        let dreamItems = document.querySelectorAll('.dream-item')
+        dreamItems.forEach(function (item) {
+          if (selectedMonth === 0 || selectedMonth === parseInt(item.getAttribute('data-month'))) {
+            item.style.display = 'block'
+          } else {
+            item.style.display = 'none'
+          }
+        })
+      }
+
+      document.getElementById('month-select').addEventListener('change', filterDreamsByMonth)
+
+      window.addEventListener('load', filterDreamsByMonth)
+    </script>
 @endsection
